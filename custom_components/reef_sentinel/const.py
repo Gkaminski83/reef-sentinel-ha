af1dc92db01cabfd37300dfcd7859904b450e850
Grzegorz Kamiński <grzegorz.kaminski.ds@gmail.com>
2026-02-12 14:57:35 +0100

    chore: change Reef Sentinel polling interval to 2 hours (7200s)

--- custom_components/reef_sentinel/const.py
+++ custom_components/reef_sentinel/const.py
@@ -2,7 +2,7 @@
 
 DOMAIN = "reef_sentinel"
 PLATFORMS = ["sensor"]
-DEFAULT_SCAN_INTERVAL = 300  # seconds
+DEFAULT_SCAN_INTERVAL = 7200  # seconds (2 hours)
 API_BASE = "https://app.reef-sentinel.com/api/ha/tank-status"
 
 CONF_API_KEY = "api_key"
